c4b0a40fb584fca5c8b554a20d06de4b4c1b666b
STRUGACH-SUS <mitakervanov@gmail.com>
2025-10-21 20:44:58 +0500

    Homework 2.cs

--- Homework 2.cs
+++ Homework 2.cs
@@ -2,23 +2,40 @@
 InputNumberInAnotherSystem();
 static void InputNumberInAnotherSystem() // Получаем данные от пользователя
 {
-    Console.Write("Please enter first number for sum: ");
+    Console.Write("Please enter the first number: ");
     string result1 = Console.ReadLine();
-    Console.Write("Please enter first system of your first number: ");
+    Console.Write("Please enter the first system of your first number: ");
     int.TryParse(Console.ReadLine(),out int firstSystem1);
-    Console.Write("Please enter second number for sum: ");
+    Console.Write("Please enter the operation with numbers: ");
+    string operation = Console.ReadLine().Trim();
+    Console.Write("Please enter the second number: ");
     string result2 = Console.ReadLine();
-    Console.Write("Please enter first system of your second number: ");
+    Console.Write("Please enter the first system of your second number: ");
     int.TryParse(Console.ReadLine(),out int firstSystem2);
-    Console.Write("Please enter end system of your sum numbers: ");
+    Console.Write("Please enter the end system of your numbers: ");
     int.TryParse(Console.ReadLine(),out int endSystem);
     CheckValidInputNumber(result1, firstSystem1, endSystem);
     CheckValidInputNumber(result2, firstSystem2, endSystem);
     int number1 = ChangeSystemOfNumberInTenSystem(result1, firstSystem1);
     int number2 = ChangeSystemOfNumberInTenSystem(result2, firstSystem2);
-    int resultInTenSystem = number1 + number2;
-    Console.WriteLine("Your sum numbers in 10 system: " + resultInTenSystem);
-    ChangeSystemOfSumNumbers(resultInTenSystem, endSystem);
+    switch (operation)
+    {
+        case "*":
+            ChangeSystemOfNumbers(number1 * number2, endSystem);
+            break;
+        case "/":
+            ChangeSystemOfNumbers(number1 / number2, endSystem);
+            break;
+        case "+":
+            ChangeSystemOfNumbers(number1 + number2, endSystem);
+            break;
+        case "-":
+            ChangeSystemOfNumbers(number1 - number2, endSystem);
+            break;
+        default:
+            Console.WriteLine("Invalid operation!");
+            break;
+    }
 }
 static void CheckValidInputNumber(string number,int firstSystem,int endSystem) // Проверка полученных данных
 {
@@ -27,13 +44,13 @@ static void CheckValidInputNumber(string number,int firstSystem,int endSystem) /
     bool invalidDataNumber = false;
     for (int i = 0; i < number.Length; i++)
     {
-        if (!(testOfCharString.Contains(number[i])) ^ (number.Substring(1).Contains('-')))
+        if (!(testOfCharString.Contains(number[i]))||(number.Substring(1).Contains('-')))
         {
             invalidDataNumber=true;
             break;
         }
     }
-    if (invalidDataNumber^!(firstSystem<=62 && firstSystem>=2)^!(endSystem<=62 && endSystem>=2))
+    if (invalidDataNumber||!(firstSystem<=62 && firstSystem>=2)||!(endSystem<=62 && endSystem>=2))
     {
         Console.WriteLine("Invalid input, try again");
         InputNumberInAnotherSystem();
@@ -41,56 +58,37 @@ static void CheckValidInputNumber(string number,int firstSystem,int endSystem) /
 }
 static int ChangeSystemOfNumberInTenSystem(string number,int firstSystem) // Перевод из одной системы счисления в другую числа пользователя
 {
-    bool negative = false;
-    string testOfCharString = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    if (number[0] == '-')
+    string testOfCharString="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if (number[0]=='-')
     {
-        number = number.Substring(1);
-        negative = true;
+        number=number.Substring(1);
     }
-    int counter = 0;
-    int numberInTenSystem = 0;
-    for (int i = number.Length-1; i >= 0; i--)
+    int counter=0;
+    int numberInTenSystem=0;
+    for (int i=number.Length-1;i>=0;i--)
     {
-        numberInTenSystem = numberInTenSystem+(testOfCharString.IndexOf(number[i]))*Convert.ToInt32(Math.Pow(firstSystem,counter));
+        numberInTenSystem=numberInTenSystem+(testOfCharString.IndexOf(number[i]))*Convert.ToInt32(Math.Pow(firstSystem,counter));
         counter++;
-    }
-    if (negative)
-    {
-        Console.WriteLine("Your number in 10 system: -" + numberInTenSystem);
-        numberInTenSystem=-numberInTenSystem;
-    }
-    else
-    {
-        Console.WriteLine("Your number in 10 system: " + numberInTenSystem);
     }
     return numberInTenSystem;
 }
-static string ChangeSystemOfSumNumbers(int sumNumbersInTenSystem, int endSystem)// Перевод из одной системы счисления в другую числа пользователя
+static string ChangeSystemOfNumbers(int sumNumbersInTenSystem,int endSystem)// Перевод из одной системы счисления в другую числа пользователя
 {
-    string result = "";
-    bool negative = false;
-    string testOfCharString = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    if (sumNumbersInTenSystem < 0)
-    {
-        sumNumbersInTenSystem =  -sumNumbersInTenSystem;
-        negative = true;
-    }
+    string result="";
+    bool negative=false;
+    string testOfCharString="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    sumNumbersInTenSystem=(sumNumbersInTenSystem<0)?-sumNumbersInTenSystem:sumNumbersInTenSystem;
     while (true)
     {
-        result = testOfCharString[sumNumbersInTenSystem % endSystem] + result;
-        if (sumNumbersInTenSystem / endSystem < endSystem)
+        result=testOfCharString[sumNumbersInTenSystem%endSystem]+result;
+        if (sumNumbersInTenSystem/endSystem<endSystem)
         {
-            sumNumbersInTenSystem = sumNumbersInTenSystem / endSystem;
-            result = testOfCharString[sumNumbersInTenSystem] + result;
+            sumNumbersInTenSystem=sumNumbersInTenSystem/endSystem;
+            result=testOfCharString[sumNumbersInTenSystem]+result;
             break;
         }
-        sumNumbersInTenSystem = sumNumbersInTenSystem / endSystem;
+        sumNumbersInTenSystem=sumNumbersInTenSystem/endSystem;
     }
-    if (negative)
-    {
-        result = "-" + result;
-    }
-    Console.WriteLine("Your sum numbers in " + endSystem +" system: " + result);
+    Console.WriteLine($"Your result number in {endSystem} system: {(negative?'-'+result:result)}");
     return result;
 } 
