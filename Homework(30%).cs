b4ea5f6e3901cbad424be9990fe2ff65684638ff
Стругач <mitakervanov@gmail.com>
2025-11-09 00:38:48 +0500

    Homework(30%).cs

--- Homework(30%).cs
+++ Homework(30%).cs
@@ -1,11 +1,12 @@
-﻿namespace Homework3
+namespace Homework3
 {
     public class CalculatorOfNumeralSystem
     {
-        private string MathematicalExpression {get; set;}
-        private int FirstNumeralSystem {get; set;}
-        private int SecondNumeralSystem {get; set;}
+        private string MathematicalExpression {get;}
+        private int FirstNumeralSystem {get;}
+        private int SecondNumeralSystem {get;}
         private string TestOfCharString = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        private List<string> MathematicalExpressionList {get;}
 
         public CalculatorOfNumeralSystem(string mathematicalExpression, string firstNumeralSystem, string secondNumeralSystem)
         {
@@ -16,8 +17,8 @@
             this.SecondNumeralSystem = SecondNumeralSystem;
             
             CheckTheCorrectInput(MathematicalExpression, this.FirstNumeralSystem, this.SecondNumeralSystem,TestOfCharString);
-            ConvertOfAnExpressionToAnotherNumberSystem(MathematicalExpression, this.FirstNumeralSystem,
-                this.SecondNumeralSystem, TestOfCharString);
+            MathematicalExpressionList=ConvertMathematicalExpressionStringToList(MathematicalExpression);
+            ConvertOfAnExpressionToAnotherNumberSystem(MathematicalExpressionList, this.FirstNumeralSystem, this.SecondNumeralSystem, TestOfCharString);
         }
 
         private static void CheckTheCorrectInput(string mathematicalExpression,int firstNumeralSystem,int secondNumeralSystem,string testOfCharString)
@@ -47,17 +48,90 @@
             }
         }
 
-        private static void ConvertOfAnExpressionToAnotherNumberSystem(string mathematicalExpression,
-            int firstNumeralSystem, int secondNumeralSystem, string testOfCharString)
+        private static List<string> ConvertMathematicalExpressionStringToList(string mathematicalExpression)
         {
+            int counter = 0;
+            List<string> ListMathematicalExpression = new List<string>();
             
-            
-            
-            
+            for (int i = 0; i < mathematicalExpression.Length; i++)
+            {
+                if (mathematicalExpression[i] == '+' || mathematicalExpression[i] == '-' ||
+                    mathematicalExpression[i] == '/' || mathematicalExpression[i] == '*')
+                {
+                    ListMathematicalExpression.Add(mathematicalExpression.Substring(counter,i-counter));
+                    ListMathematicalExpression.Add(mathematicalExpression[i].ToString());
+                    counter = i + 1;
+                }
+            }
+            ListMathematicalExpression.Add(mathematicalExpression.Substring(counter));
+            if (ListMathematicalExpression[0] == "")
+            {
+                ListMathematicalExpression[0] = "0";
+            }
+
+            return ListMathematicalExpression;
         }
         
+        private static void ConvertOfAnExpressionToAnotherNumberSystem(List<string> mathematicalExpression,int firstNumeralSystem, int secondNumeralSystem, string testOfCharString)
+        {
+            for (int i = 0; i < mathematicalExpression.Count; i++)
+            {
+                if (mathematicalExpression[i] == "*")
+                {
+                    mathematicalExpression[i] = MultiOfTwoNumbers(mathematicalExpression[i-1],mathematicalExpression[i+1],firstNumeralSystem,testOfCharString);
+                    mathematicalExpression.RemoveAt(i-1);
+                    mathematicalExpression.RemoveAt(i+1);
+                }
+                
+                
+                
+            }
+            for (int i = 0; i < mathematicalExpression.Count; i++)
+            {
+                if (mathematicalExpression[i] == "+")
+                {
+                    mathematicalExpression[i] = SumOfTwoNumbers(mathematicalExpression[i-1],mathematicalExpression[i+1],firstNumeralSystem,testOfCharString);
+                    mathematicalExpression.RemoveAt(i-1);
+                    mathematicalExpression.RemoveAt(i+1);
+                }
+                
+                
+            }
+            
+        }
+
+        private static string SumOfTwoNumbers(string firstNumber, string secondNumber, int firstNumeralSystem, string testOfCharString)//В этом методе надо добавить чтобы у меньшего по длине числа добавлялись спереди нули
+        {
+           
+            string transfer="";
+            int maxLength=(firstNumber.Length>secondNumber.Length)?firstNumber.Length:secondNumber.Length;//поидее нахуй не нужно если дополняем до нулей
+            string result = string.Create(maxLength, '0', (c, b) => { for (int i = 0; i < c.Length; i++) c[i] = '0'; });//Тут не результат строку надо брать а добавлять нули к числам
+            for (int i = maxLength - 1; i >= 0; i--)
+            {   
+                result = testOfCharString[((testOfCharString.IndexOf(firstNumber[i]) + testOfCharString.IndexOf(secondNumber[i])+testOfCharString.IndexOf(transfer))%firstNumeralSystem)] + result;
+                transfer = testOfCharString[((testOfCharString.IndexOf(firstNumber[i]) + testOfCharString.IndexOf(secondNumber[i])+testOfCharString.IndexOf(transfer))/firstNumeralSystem)].ToString();
+            }
+            result=(transfer!="0")?transfer+result:result;
+            return result;
+        }
         
-        
+        private static string MultiOfTwoNumbers(string firstNumber, string secondNumber, int firstNumeralSystem, string testOfCharString)
+        {
+            string result = "",resultIntermediative = "",transfer="";
+
+            for (int i = firstNumber.Length - 1; i >= 0; i--)
+            {   
+                for (int j = secondNumber.Length - 1; j >= 0; j--)
+                {
+                    resultIntermediative = testOfCharString[(testOfCharString.IndexOf(firstNumber[i]) * testOfCharString.IndexOf(secondNumber[j])+testOfCharString.IndexOf(transfer))%firstNumeralSystem] + resultIntermediative;
+                    transfer = testOfCharString[(testOfCharString.IndexOf(firstNumber[i]) * testOfCharString.IndexOf(secondNumber[j])+testOfCharString.IndexOf(transfer))/firstNumeralSystem].ToString();
+                }
+                resultIntermediative=(transfer!="0")?transfer+resultIntermediative:resultIntermediative;
+                result=SumOfTwoNumbers(result,resultIntermediative,firstNumeralSystem,testOfCharString);
+                resultIntermediative = "";
+            }
+            return result;
+        }
     }
     
     
